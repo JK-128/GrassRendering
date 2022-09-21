4f41676d4dc2a9ef2294c786050ffddeedf1da81
JoeKeep <19701770@students.lincoln.ac.uk>
2022-09-21 21:13:38 +0100

    Added GLM to the project.

--- Grass Rendering/Code/Source.cpp
+++ Grass Rendering/Code/Source.cpp
@@ -7,6 +7,10 @@
 #include "ImGuiWrapper.h"
 #include "Console.h"
 
+//GLM include test
+#include "../Dependencies/glm/glm/glm.hpp"
+#include "../Dependencies/glm/glm/matrix.hpp"
+
 int WinMain()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
@@ -14,6 +18,12 @@ int WinMain()
 	logMessage("Grass Renderer Program Started.", "MAIN");
 
 	Window window(800, 800, "Test Window", true, true);
+
+	glm::vec3 one   = glm::vec3(1, 1, 1);
+	glm::vec3 two   = glm::vec3(2, 2, 2);
+	glm::vec3 three = one * two;
+
+	logMessage("GLM test: " + std::to_string(three.x), "MAIN");
 
 	if (window.isCreated())
 	{
@@ -44,8 +54,6 @@ int WinMain()
 /*
 TO DO:
 ------
-+ clean up main function/source file.
-+ Create a hierarchy for IMGUI/UI objects.
 + Add GLM to the project.
 + Get a plane loaded in.
 + Create some kind of simple camera that allows for WASD movement.
